39cba07242f018c2da7221c0c5d8351be20a524c
mohanbarani <53135851+mohanbarani@users.noreply.github.com>
2026-07-06 15:02:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD Pipeline! Automated the push event by configuring webhook"
+    return "Hello from Jenkins CI/CD Pipeline! Automated the push event by configuring webhook, now check!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
